758aeaa5323f76187b12b21bcdb29c2c2dbd96b7
M. Rezky Revansyah Suprihono <rezky.revansyah@gmail.com>
2024-11-19 10:52:47 +0700

    add header and navbar

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+/**
+ * @copyright 2024 rezkyrevansyah
+ * @license Apache-2.0
+ */
+
+/**
+ * Components
+ */
+import Header from "./components/Header";
+
+const App = () => {
+    return (
+        <Header />
+    )
+}
+
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,24 @@
+/**
+ * @copyright 2024 rezkyrevansyah
+ * @license Apache-2.0
+ */
+
+/**
+ * Node Modules
+ */
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+/**
+ * Components
+ */
 import './index.css'
+
+/**
+ * CSS Link
+ */
 import App from './App.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
